0fd873a18a900e76af64550a45eae3f3d6210d13
weeniemount <59868100+weeniemount@users.noreply.github.com>
2025-09-21 13:56:12 +0200

    make deploy commands go through sub dirs

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,13 +1,30 @@
 const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
+const path = require('node:path');
 dotenv = require('dotenv');
 dotenv.config();
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+function getCommandFiles(dir) {
+	let files = [];
+	const entries = fs.readdirSync(dir, { withFileTypes: true });
+
+	for (const entry of entries) {
+		const fullPath = path.join(dir, entry.name);
+		if (entry.isDirectory()) {
+			files = files.concat(getCommandFiles(fullPath));
+		} else if (entry.isFile() && entry.name.endsWith('.js')) {
+			files.push(fullPath);
+		}
+	}
+
+	return files;
+}
+
+const commandFiles = getCommandFiles('./commands');
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(path.resolve(file));
 	const json = command.data.toJSON();
 
 	const extras = {
@@ -18,12 +35,16 @@ for (const file of commandFiles) {
 
 	commands.push(json);
 }
-
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
 	try {
-		console.log('Started refreshing application (/) commands.');
+		let thecountofemall = 0;
+		for (const file of commandFiles) {
+			thecountofemall++;
+		}
+
+		console.log(`Started refreshing ${thecountofemall} application (/) commands.`);
 
 		// Send array of objects, not a string
 		await rest.put(
@@ -31,7 +52,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 			{ body: commands }
 		);
 
-		console.log('Successfully reloaded application (/) commands.');
+		console.log(`Successfully reloaded ${thecountofemall} application (/) commands.`);
 	} catch (error) {
 		console.error(error);
 	}
