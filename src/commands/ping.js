8e72cd04c75225f37e908eb47df08b61b259d9f1
weeniemount <59868100+weeniemount@users.noreply.github.com>
2025-09-21 13:59:33 +0200

    better ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,20 +1,29 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with Pong!'),
-  async execute(interaction) {
-    const pingEmbed = new EmbedBuilder()
-        .setColor(0xb03000)
-        .setTitle("pong!")
-        .setAuthor(
-            { 
-                name: "weenBot"
-            }
-        )
-
-
-    await interaction.reply({embeds: [pingEmbed]});
-  },
+	data: new SlashCommandBuilder({ integration_types: [0,1] })
+		.setName('ping')
+		.setDescription('replies with Pong!'),
+	async execute(interaction) {
+		const startTime = Date.now();
+		
+		const pingembed = new EmbedBuilder()
+			.setColor(0xb03000)
+			.setTitle(`Pong!`)
+			.addFields(
+				{name: `${interaction.client.ws.ping} ms`, value: "     "}
+			)
+		const pingembedsecret = new EmbedBuilder()
+			.setColor(0xb03000)
+			.setTitle(`Pong!`)
+			.addFields(
+				{name: "${interaction.client.ws.ping} (you got an easter egg! i love interaction.client.ws.ping ms) ms", value: "     "}
+			)
+		const randomNumber = Math.floor(Math.random() * 1000) + 1;
+		if (randomNumber == 1) {
+			await interaction.reply({embeds: [pingembedsecret]});
+		} else {
+			await interaction.reply({embeds: [pingembed]});
+		}
+	},
 };
