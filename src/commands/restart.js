7d4b9cedd1b17f371e880332cff64605398844bc
weeniemount <59868100+weeniemount@users.noreply.github.com>
2025-10-01 17:41:19 +0200

    Update restart.js

--- src/commands/restart.js
+++ src/commands/restart.js
@@ -5,7 +5,7 @@ module.exports = {
 		.setName('restart')
 		.setDescription('restarts the bot (DEV COMMAND! GUILD SPECIFIC, ONLY WORKS IF YOU STARTED THE BOT WITH PM2.'),
 	async execute(interaction) {
-		if (interaction.user.id == ownerUserID) {
+		if (interaction.user.id == process.env.OWNER_USER_ID) {
 			await interaction.reply('brb');
 			process.exit();
 		} else {
